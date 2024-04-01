f05de89574738d1e427dd2acdb7a054bd630c2ad
NikPeg <peganov.nik@gmail.com>
2024-04-01 15:43:24 +0300

    normal buttons

---
+++ final/buttons.py
@@ -0,0 +1,8 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+import config
+
+FEEDBACK = InlineKeyboardButton(text='👌Оставить отзыв', callback_data='give_feedback')
+ABOUT = InlineKeyboardButton(text='ℹ️Узнать о frAId', callback_data='info')
+SOS = InlineKeyboardButton(text='🆘Поддержка', url=config.SOS_URL)
+BACK = InlineKeyboardButton(text='👈Назад', callback_data='return')
