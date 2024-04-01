ae65f5ce92a408893158693e2a84e43723c204a7
NikPeg <peganov.nik@gmail.com>
2024-04-02 00:02:35 +0300

    Revert "Normal gpt proxy"
    This reverts commit 492a292cf95a79b2bc9061cd6469d29460c06ee8.

--- final/handlers/users/start_command.py
+++ final/handlers/users/start_command.py
@@ -11,8 +11,6 @@ from database.users_db import add_new_user
 from keyboards.keyboards import start_markup, return_markup
 from loader import dp, bot
 from messages import HELP, START, PROMPT, NEW_PROMPT
-from loader import gpt
-import buttons
 
 
 class UserState(StatesGroup):
@@ -38,7 +36,7 @@ async def info_handler(call: types.CallbackQuery):
     await call.message.edit_text(HELP, reply_markup=return_markup())
     await bot.send_message(
         ADMIN_ID,
-        messages.BUTTON_PRESSED.format(call.message.chat.id, call.message.chat.username, buttons.ABOUT.text),
+        messages.BUTTON_PRESSED.format(call.message.chat.id, call.message.chat.username, "ℹ️Узнать о frAId"),
     )
 
 
@@ -47,7 +45,7 @@ async def return_handler(call: types.CallbackQuery):
     await call.message.edit_text(START, reply_markup=start_markup())
     await bot.send_message(
         ADMIN_ID,
-        messages.BUTTON_PRESSED.format(call.message.chat.id, call.message.chat.username, buttons.BACK.text),
+        messages.BUTTON_PRESSED.format(call.message.chat.id, call.message.chat.username, "Назад"),
     )
 
 
@@ -78,11 +76,12 @@ async def user_gpt_req_handler(message: types.Message):
     await asyncio.create_task(create_user_req(message.chat.id, message.chat.username, req_text))
 
 
-async def create_user_req(user_id, user_name, request_text):
-    bot_answer = gpt.ask(request_text)
-    add_new_message(user_id, request_text, bot_answer)
-    await bot.send_message(user_id, bot_answer)
+async def create_user_req(user_id, user_name, req_text):
+    from gpt_func import gpt_ask_func
+    bot_req = gpt_ask_func(req_text)
+    add_new_message(user_id, req_text, bot_req)
+    await bot.send_message(user_id, bot_req)
     await bot.send_message(
         ADMIN_ID,
-        messages.MESSAGE_SENT.format(user_id, user_name, request_text, bot_answer),
+        messages.MESSAGE_SENT.format(user_id, user_name, req_text, bot_req),
     )
