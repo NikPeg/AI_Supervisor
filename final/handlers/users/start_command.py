4625bf6af47eb48a13120965f37390812a84587f
NikPeg <peganov.nik@gmail.com>
2024-04-01 12:34:26 +0300

    normal messages

--- final/handlers/users/start_command.py
+++ final/handlers/users/start_command.py
@@ -3,7 +3,7 @@ import asyncio
 from loader import dp,bot
 from aiogram import types
 from keyboards.keyboards import start_markup,return_markup
-from messages import fr_mess,start_mess,start_mess_2,new_mess
+from messages import HELP,START,PROMPT,NEW_PROMPT
 from aiogram.dispatcher.filters.state import State,StatesGroup
 from database.sessia_db import create_new_sessia
 from database.mess_db import add_new_message
@@ -20,35 +20,35 @@ class User_(StatesGroup):
 @dp.message_handler(commands=['start'],state="*")
 async def start_command_handler(message:types.Message):
     add_new_user(message.chat.id, message.chat.username)
-    await bot.send_message(message.chat.id, start_mess,reply_markup=start_markup())
+    await bot.send_message(message.chat.id, START, reply_markup=start_markup())
     create_new_sessia(message.chat.id)
-    await bot.send_message(message.chat.id,start_mess_2)
+    await bot.send_message(message.chat.id, PROMPT)
     await bot.send_message(admin_id, f"Пользователь {message.chat.id} c ником {message.chat.username} нажал кнопку /start.")
     await User_.gpt_req.set()
 
 @dp.callback_query_handler(text='info',state="*")
 async def info_handler(call:types.CallbackQuery):
-    await call.message.edit_text(fr_mess,reply_markup=return_markup())
+    await call.message.edit_text(HELP, reply_markup=return_markup())
     await bot.send_message(admin_id, f"Пользователь {call.message.chat.id} c ником {call.message.chat.username} нажал кнопку ℹ️Узнать о frAId.")
 
 
 @dp.callback_query_handler(text='return',state="*")
 async def return_handler(call:types.CallbackQuery):
-    await call.message.edit_text(start_mess,reply_markup=start_markup())
+    await call.message.edit_text(START, reply_markup=start_markup())
     await bot.send_message(admin_id, f"Пользователь {call.message.chat.id} c ником {call.message.chat.username} нажал кнопку Назад.")
 
 
 
 @dp.message_handler(commands=['help'],state="*")
 async def help_message_handler(message:types.Message):
-    await bot.send_message(message.chat.id, fr_mess)
-    await bot.send_message(message.chat.id, start_mess_2)
+    await bot.send_message(message.chat.id, HELP)
+    await bot.send_message(message.chat.id, PROMPT)
     await bot.send_message(admin_id, f"Пользователь {message.chat.id} c ником {message.chat.username} нажал кнопку /help.")
 
 
 @dp.message_handler(commands=['new'],state="*")
 async def help_message_handler(message:types.Message):
-    await bot.send_message(message.chat.id, new_mess)
+    await bot.send_message(message.chat.id, NEW_PROMPT)
     create_new_sessia(message.chat.id)
     await bot.send_message(admin_id, f"Пользователь {message.chat.id} c ником {message.chat.username} нажал кнопку /new.")
     await User_.gpt_req.set()
