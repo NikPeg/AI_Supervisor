85f46fc50e68d4a7cdb625d07ee0c76b302617e6
NikPeg <peganov.nik@gmail.com>
2024-04-02 11:49:26 +0300

    fetchone

--- final/database/message_db.py
+++ final/database/message_db.py
@@ -15,6 +15,7 @@ def add_new_message(user_id, user_req, bot_req):
 def get_conversation_by_user(user_id):
     print("!!!get_conversation_by_user")
     session_id = get_user_session_id(user_id)
-    res = cursor.execute("SELECT * FROM Message WHERE user_id=? AND session_id=?", (user_id, session_id,))
+    cursor.execute("SELECT * FROM Message WHERE user_id=? AND session_id=?", (user_id, session_id,))
+    res = cursor.fetchone()
     print(f"SELECT * FROM Message WHERE user_id={user_id} AND session_id={session_id}")
     print(config.ADMIN_ID, res)
