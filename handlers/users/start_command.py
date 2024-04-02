2942c6ca2de9bb7a083ff5bd1735f4efd763952c
NikPeg <peganov.nik@gmail.com>
2024-04-02 18:14:27 +0300

    Markdown

--- handlers/users/start_command.py
+++ handlers/users/start_command.py
@@ -81,7 +81,7 @@ async def create_user_req(user_id, user_name, request_text):
     conversation = get_conversation_by_user(user_id)
     bot_answer = gpt.ask(request_text, conversation)
     add_new_message(user_id, request_text, bot_answer)
-    await bot.send_message(user_id, bot_answer, parse_mode="MarkdownV2")
+    await bot.send_message(user_id, bot_answer, parse_mode="Markdown")
     await bot.send_message(
         ADMIN_ID,
         messages.MESSAGE_SENT.format(user_id, user_name, request_text, bot_answer),
