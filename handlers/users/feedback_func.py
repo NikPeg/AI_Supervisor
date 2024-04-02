d8fa4f70e1b48b591327dcea94ab63b5971c984a
NikPeg <peganov.nik@gmail.com>
2024-04-02 15:03:03 +0300

    FEEDBACK_PERIOD

--- handlers/users/feedback_func.py
+++ handlers/users/feedback_func.py
@@ -30,15 +30,16 @@ async def feedback_handler(message: types.Message, state: FSMContext):
     delete_user_from_feedback(message.chat.id)
 
 
+FEEDBACK_PERIOD = 30 * 60
+
+
 async def start_feed_back():
     while True:
         all_users = get_all_feed_back_users()
         for user in all_users:
             try:
                 await bot.send_message(user[0], text=messages.FEEDBACK_ASK, reply_markup=feedback_markup())
-                # delete_user_from_feedback(user[0])
             except Exception as e:
                 await bot.send_message(ADMIN_ID, text=e)
                 continue
-        # await asyncio.sleep(8640)
-        await asyncio.sleep(5)
+        await asyncio.sleep(FEEDBACK_PERIOD)
