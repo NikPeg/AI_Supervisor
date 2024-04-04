5cff4dede1fa1ccc3341b947096fcb17f2f7c73a
NikPeg <peganov.nik@gmail.com>
2024-04-04 14:03:50 +0300

    html

--- handlers/users/commands.py
+++ handlers/users/commands.py
@@ -90,7 +90,7 @@ async def create_user_req(user_id, user_name, request_text):
     try:
         new_answer = markdown_to_html(bot_answer)
         print(new_answer)
-        await bot.send_message(user_id, markdown_to_html(bot_answer), parse_mode=ParseMode.MARKDOWN)
+        await bot.send_message(user_id, markdown_to_html(bot_answer), parse_mode=ParseMode.HTML)
     except Exception as e:
         print(e)
         await bot.send_message(user_id, bot_answer)
