baf68b98ec71bbfbc532a0de7b5b7932a9a1ebcd
NikPeg <peganov.nik@gmail.com>
2024-04-04 19:07:41 +0300

    answer_message_handler

--- handlers/admin/commands.py
+++ handlers/admin/commands.py
@@ -9,18 +9,4 @@ from handlers.common import create_user_req
 from loader import dp, bot
 
 
-@dp.message_handler(commands=['answer'], state="*")
-async def help_message_handler(message: types.Message):
-    if not message.reply_to_message:
-        bot.send_message(ADMIN_ID, messages.WRONG_MESSAGE)
-        return
-    user_id = int(message.reply_to_message.text.split()[1])
-    username = message.reply_to_message.text.split()[4][1:]
-    request_text = message.reply_to_message.text
-    try:
-        await asyncio.create_task(create_user_req(user_id, username, request_text))
-    except openai.BadRequestError as e:
-        await bot.send_message(message.chat.id, messages.WAIT)
-        await bot.send_message(ADMIN_ID, messages.WAIT + e)
-    except Exception as e:
-        await bot.send_message(ADMIN_ID, messages.UNKNOWN_ERROR + e)
+
