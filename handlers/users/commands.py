15797f938a96dc82d31ef16c788ca103b51fc063
NikPeg <peganov.nik@gmail.com>
2024-04-04 13:45:19 +0300

    markdown_to_telegram not admin

--- handlers/users/commands.py
+++ handlers/users/commands.py
@@ -88,9 +88,9 @@ async def create_user_req(user_id, user_name, request_text):
     )
     thread_id = get_thread_id(user_id)
     await gpt.add_message(thread_id, request_text)
-    bot_answer = markdown_to_telegram(await gpt.get_answer(thread_id))
+    bot_answer = await gpt.get_answer(thread_id)
     try:
-        await bot.send_message(user_id, bot_answer, parse_mode='Markdown')
+        await bot.send_message(user_id, markdown_to_telegram(bot_answer), parse_mode='Markdown')
     except Exception as e:
         print(e)
         await bot.send_message(user_id, bot_answer)
