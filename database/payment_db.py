76815adb99ed494ca3d39be558f47900780f62bb
NikPeg <peganov.nik@gmail.com>
2024-04-13 15:15:43 +0300

    Fix user_id

--- database/payment_db.py
+++ database/payment_db.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 def check_payment(user_id):
-    cursor.execute("SELECT paid_by FROM User WHERE user_id=?", (user_id,))
+    cursor.execute("SELECT paid_by FROM User WHERE id=?", (user_id,))
     paid_by = cursor.fetchone()[0]
     print("Оплачено до:", paid_by)
     if paid_by >= datetime.now():
