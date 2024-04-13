6571ae03a615ccf1d182565dbd0a5d2e94153652
NikPeg <peganov.nik@gmail.com>
2024-04-13 15:22:04 +0300

    Delete prints

--- database/payment_db.py
+++ database/payment_db.py
@@ -8,7 +8,6 @@ DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 def check_payment(user_id):
     cursor.execute("SELECT paid_by FROM User WHERE id=?", (user_id,))
     paid_by = datetime.strptime(cursor.fetchone()[0], DATE_FORMAT)
-    print("Оплачено до:", paid_by)
     if paid_by >= datetime.now():
         return True
     return False
