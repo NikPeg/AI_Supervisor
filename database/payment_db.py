bc53385a599362f8ecec196b1d8dc2f27e12f708
NikPeg <peganov.nik@gmail.com>
2024-04-13 15:18:57 +0300

    strptime

--- database/payment_db.py
+++ database/payment_db.py
@@ -2,9 +2,11 @@ from . import database, cursor
 from datetime import datetime
 
 
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
 def check_payment(user_id):
     cursor.execute("SELECT paid_by FROM User WHERE id=?", (user_id,))
-    paid_by = cursor.fetchone()[0]
+    paid_by = datetime.strptime(cursor.fetchone()[0], DATE_FORMAT)
     print("Оплачено до:", paid_by)
     if paid_by >= datetime.now():
         return True
