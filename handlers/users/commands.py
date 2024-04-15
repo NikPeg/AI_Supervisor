017f68c44ec442aa6a969d9337fa561cad983555
NikPeg <peganov.nik@gmail.com>
2024-04-16 00:10:12 +0300

    right price

--- handlers/users/commands.py
+++ handlers/users/commands.py
@@ -57,7 +57,7 @@ async def return_handler(call: types.CallbackQuery):
 
 @dp.callback_query_handler(text='payment', state="*")
 async def payment_handler(call: types.CallbackQuery):
-    link = client.create_order(100, Currency.RUB, "Бот").url
+    link = client.create_order(config.PRICE, Currency.RUB, "Бот").url
     await call.message.edit_text(messages.PAYMENT_LINK.format(link), reply_markup=start_markup())
     await bot.send_message(
         ADMIN_ID,
@@ -66,7 +66,7 @@ async def payment_handler(call: types.CallbackQuery):
     await UserState.payment.set()
 
 
-@dp.message_handler(text="*", state=UserState.payment)
+@dp.message_handler(state=UserState.payment)
 async def paid_handler(message: types.Message):
     await bot.send_message(message.chat.id, messages.PAYMENT_PROCESS, reply_markup=start_markup())
     await bot.send_message(
