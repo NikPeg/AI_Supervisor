c38d4b4980d8f0694fb4db9fd25a470f931ab687
NikPeg <peganov.nik@gmail.com>
2024-04-17 23:59:11 +0300

    admin command

--- handlers/users/commands.py
+++ handlers/users/commands.py
@@ -113,25 +113,6 @@ async def help_message_handler(message: types.Message):
     await UserState.gpt_request.set()
 
 
-@dp.message_handler(commands=['answer'], state="*")
-async def answer_message_handler(message: types.Message):
-    if message.chat.id != ADMIN_ID:
-        return
-    if not message.reply_to_message:
-        await bot.send_message(ADMIN_ID, messages.WRONG_MESSAGE)
-        return
-    user_id = int(message.reply_to_message.text.split()[1])
-    username = message.reply_to_message.text.split()[4][1:]
-    request_text = " ".join(message.reply_to_message.text.split()[7:])
-    try:
-        await asyncio.create_task(create_user_req(user_id, username, request_text))
-    except openai.BadRequestError as e:
-        await bot.send_message(message.chat.id, messages.WAIT)
-        await bot.send_message(ADMIN_ID, messages.WAIT + e)
-    except Exception as e:
-        await bot.send_message(ADMIN_ID, messages.UNKNOWN_ERROR + e)
-
-
 @dp.message_handler(state=UserState.gpt_request)
 @dp.message_handler(state=default_state)
 async def user_gpt_req_handler(message: types.Message):
