9cc595eb5bd22d066fa58126f92836ded181e4d8
NikPeg <peganov.nik@gmail.com>
2024-04-17 19:30:29 +0300

    logs

--- database/payment_db.py
+++ database/payment_db.py
@@ -6,10 +6,14 @@ DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
 def check_subscribed(user_id):
+    print("Проверяю подписку...")
     cursor.execute("SELECT register FROM User WHERE id=?", (user_id,))
     register = datetime.datetime.strptime(cursor.fetchone()[0], DATE_FORMAT)
+    print("Дата регистрации:", register)
     if register <= datetime.datetime.now() + datetime.timedelta(days=2):
+        print("Действует пробный период", register)
         return True
+    print("Проверяем подписку", register)
     cursor.execute("SELECT subscribed FROM User WHERE id=?", (user_id,))
     subscribed = bool(cursor.fetchone()[0])
     return subscribed
